ed6dd034b0ffe9e13fb0fd70233171da532f0941
OttoVazquez1 <juanvazquezconforti@gmail.com>
2022-04-13 21:56:01 -0300

    Propiedades en descripcion

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,12 +4,22 @@ import ItemCount from "../ItemCount/ItemCount";
 
 function ItemDetail({ producto }) {
   
+
  
   return (
     <>
     <div>{producto.name}</div>
     <div>{producto.price}</div>
     <div><img src={producto.foto} alt="Foto del producto" className="w-50"/></div>
+    <div>
+      <ul>
+        {
+          producto.descripcion.map((item)=> {
+            return <li key={Number.toString()}>{item}</li>
+          })
+        }
+      </ul>
+    </div>
     <ItemCount stock={producto.stock} inicial={1} producto={producto} id={producto.id} />
     </>
   )
